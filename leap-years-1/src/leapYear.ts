d8506e9ba003946725893ed3eddb66f39f06676e
jreyno77 <jreysteven1@gmail.com>
2026-03-04 11:58:01 -0700

    Date parsing is a work in progress for now

--- leap-years-1/src/leapYear.ts
+++ leap-years-1/src/leapYear.ts
@@ -3,6 +3,6 @@ import * as zod  from 'zod'
 type Year = number | string | Date;
 const date = zod.iso.date();
 export const isLeapYear: (year: Year) => boolean = (year: Year) => {
-    date.parse(year)
+    // date.parse(year);
     return false;
 };
